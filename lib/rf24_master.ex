1ddaad970778ad581dcab08b878305eed258b358
harmoN <dougwright1@gmail.com>
2016-03-17 23:33:35 -0400

    working pretty well, time to integrate with a phoenix app!

--- lib/rf24_master.ex
+++ lib/rf24_master.ex
@@ -1,24 +1,64 @@
-defmodule RfMaster do
-  alias RfMaster.Msg
 
-  def startIPC() do
-    {:ok, s} = :enm.pair
-    :enm.bind(s, "ipc:///tmp/rf24d.ipc")
-    {:ok, s}
+defmodule RfMaster.State do
+  @doc ~S"""
+  socket :: %Socket
+  count :: integer
+  """
+defstruct socket: nil, received: 0, sent: 0, socket_address: nil, handler: {nil, nil}
+  #@type t :: %__MODULE__{socket: reference, count: integer, socket_address: String.t, handler: tuple}
+end
+
+defmodule RfMaster do
+alias RfMaster.State
+use GenServer
+
+  def start_link() do
+    {mod, fun} = Application.get_env :rf24_master, :msg_handler, {__MODULE__, :default_msg_handler}
+    GenServer.start_link(__MODULE__, %State{ handler: {mod, fun}, socket_address: Application.get_env(:rf24_master, :socket_address, "ipc:///tmp/rf24d.ipc" ) }, name: __MODULE__)
+  end 
+
+  def sendMsg(pid, jsonMsg) do
+    GenServer.cast(pid, {:send, jsonMsg})
   end
 
- def createMsg(type, node_id, msg) do
-     Poison.encode!(%Msg{type: type, node_id: node_id, msg: msg})
- end
+  def recivedCount(pid) do
+    GenServer.call(pid, :counts)
+  end
 
- def greenMsg(s) do
-    msg = createMsg(2,0o01, "HI")
-   :enm.send(s, msg)
- end
+   def init(%State{} = state) do
+    require Logger
+    {:ok, nn_sock} = :enm.pair
+    :enm.bind(nn_sock, state.socket_address )
+    Logger.info("Bound NN pair socket to: #{state.socket_address}")
+    {:ok, %{state | socket: nn_sock }}
+  end
 
- def redMsg(s) do
-   msg = createMsg(1,0o01, "HI")
-   :enm.send(s, msg)
- end
+  def handle_call(:counts, _from,  %State{} = state) do
+    {:reply, %{received: state.received, sent: state.sent}, state}
+  end
+
+  def handle_cast({:send, jsonMsg}, %State{socket: socket} = state) do
+    :enm.send(socket, jsonMsg)
+    sent_one = state.sent + 1 
+    {:noreply, %State{state | sent: sent_one}}
+  end
+
+  def handle_info({_,_,msg}, %State{socket: socket, handler: {mod, fun}} = state) do
+    require Logger
+    received_one = state.received + 1
+    jsonMsg = String.strip(msg) |> Poison.decode!
+    apply(mod,fun,[jsonMsg])
+    {:noreply, %State{state | received: received_one}}
+  end
+
+  def terminate(_reason, %State{socket: socket} = state) when socket != nil do
+    require Logger
+    Logger.info("closing NN socket")
+    :ok = :enm.close(socket)
+  end
+
+  def default_msg_handler(msg) do
+    IO.inspect msg
+  end
 
 end
--- lib/rf_msg.ex
+++
@@ -1,4 +0,0 @@
-defmodule RfMaster.Msg do
-  @derive [Poison.Encoder]
-  defstruct [:type, :node_id, :msg]
-end
